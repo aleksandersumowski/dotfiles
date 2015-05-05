12bb99f83a8f9c7461063ce1945436e63c4d6ed0
Aleksander Sumowski <asumowsk@thoughtworks.com>
2015-05-05 23:21:52 +0100

    new vinyasa injections

--- profiles.clj
+++ profiles.clj
@@ -1,24 +1,24 @@
-{:user  {:plugins  [[lein-pprint "1.1.1"]
+{:user  {:plugins  [[cider/cider-nrepl "0.8.2"]
+                    [lein-pprint "1.1.1"]
                     [lein-try "0.4.3"]]
          :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                        [org.clojure/tools.trace "0.7.8"]
                         [redl "0.2.4"]
                         [alembic "0.3.2"]
                         [spyscope "0.1.5"]
                         [leiningen #=(leiningen.core.main/leiningen-version)]
-                        [im.chit/vinyasa "0.3.3"]]
+                        [im.chit/vinyasa "0.3.4"]]
          :injections [(require '[vinyasa.inject :as inject])
                       (inject/in
                         ;; the default injected namespace is `.`
 
                         ;; note that `:refer, :all and :exclude can be used
                         [vinyasa.inject :refer [inject [in inject-in]]]
-
-                        ;; imports all functions in vinyasa.pull
                         [alembic.still :all]
-
                         ;; inject into clojure.core
                         clojure.core
                         [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
+                        [clojure.tools.trace :all]
                         [clojure.repl doc source]
 
                         ;; inject into clojure.core with prefix
