d12ac8f2a06f129f06fa4974eae7e55cb714d838
Aleksander Sumowski <asumowsk@thoughtworks.com>
2015-05-05 23:14:44 +0100

    vinyasa.[pull, lein] -> alembic.still

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,10 @@
 {:user  {:plugins  [[lein-pprint "1.1.1"]
                     [lein-try "0.4.3"]]
          :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                        [redl "0.2.4"]
+                        [alembic "0.3.2"]
+                        [spyscope "0.1.5"]
+                        [leiningen #=(leiningen.core.main/leiningen-version)]
                         [im.chit/vinyasa "0.3.3"]]
          :injections [(require '[vinyasa.inject :as inject])
                       (inject/in
@@ -9,7 +13,8 @@
                         ;; note that `:refer, :all and :exclude can be used
                         [vinyasa.inject :refer [inject [in inject-in]]]
 
-                        [vinyasa.pull :all]
+                        ;; imports all functions in vinyasa.pull
+                        [alembic.still :all]
 
                         ;; inject into clojure.core
                         clojure.core
