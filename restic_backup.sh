8ff84405defedca0f7c96268818c5dfd31e92477
Aleksander Sumowski <58295557+aleksander-newstore@users.noreply.github.com>
2020-05-12 20:35:49 +0200

    updated restic backup script + mac scheduling

--- restic_backup.sh
+++ restic_backup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 unset HISTFILE
 source ~/.aws_restic_credentials
 export RETENTION_DAYS=7
@@ -6,7 +8,7 @@ export RETENTION_MONTHS=6
 export RETENTION_YEARS=3
 export AWS_BUCKET=s3:s3.amazonaws.com/aleks-backup-restic
 
-restic -r $AWS_BUCKET backup ~/Sync
-restic -r $AWS_BUCKET check
-restic -r $AWS_BUCKET forget --prune --cleanup-cache
-restic -r $AWS_BUCKET check
+/usr/local/bin/restic -r $AWS_BUCKET backup ~/Sync
+/usr/local/bin/restic -r $AWS_BUCKET check
+/usr/local/bin/restic -r $AWS_BUCKET forget --prune --cleanup-cache
+/usr/local/bin/restic -r $AWS_BUCKET check
