8a5aa6435ffe42e42125efd2ee05050da97afdd9
Aleksander Sumowski <github@sumowski.pl>
2023-12-01 21:47:25 +0100

    clean astro mess

--- config/nvims/lazyvim/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-vim.opt.foldenable = false
-vim.opt.shada = vim.opt.shada + "r/mnt/extdisk"
-vim.g.maplocalleader = ","
-
--- vim.g['conjure#mapping#log_jump_to_latest'] = true
