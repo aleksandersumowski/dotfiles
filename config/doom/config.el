2fecb6f647a62a65145ddc2baf54dd501065f780
Aleksander Sumowski <github@sumowski.pl>
2024-02-21 17:11:04 +0100

    bat gets nord theme

--- config/doom/config.el
+++ config/doom/config.el
@@ -74,3 +74,10 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+ ;; (define-key evil-normal-state-map "<leader>-e" 'treemacs)
+(map! :map evil-normal-state-map
+      :leader
+      :n "e"
+      #'treemacs-find-file
+      )
